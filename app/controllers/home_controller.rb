e63cf2e41bfce4b6d32579977f75994aae27298f
Lennart Prelle <me@prellele.de>
2013-05-17 15:30:16 +0200

    fix 'not displaying date of first @newest_trackings on dashbord'

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,7 @@ class HomeController < ApplicationController
 
     @todays_trackings = current_user.trackings.where("start_time >= ?", Date.today)
     @yesterdays_trackings = current_user.trackings.where("start_time > ? AND start_time < ?", Date.today-1.day, Date.today)
-    @newest_trackings = current_user.trackings.where("created_at >= ? AND start_time < ?", Date.today-1.day, Date.today-1.day)
+    @newest_trackings = current_user.trackings.where("updated_at >= ? AND start_time < ?", Date.today-1.day, Date.today-1.day)
     @projects = current_user.projects
   end
 end
