7e7f74e749e84a529af8ad4e806b129395e1b95e
Lennart Prelle <me@prellele.de>
2013-05-23 13:35:50 +0200

    refactor home/index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,24 @@
 class HomeController < ApplicationController
   # GET /
   def index
-    @greeting = ["Hey there","Bon dia","Aloha","Hallo","Hi","Ola","Hola","Namaste", "Moin Moin"].sample
-    @show_actions = true
+    @greeting = greeting()
+    set_data_for_new_tracking()
     
-    @tracking = Tracking.new(project_id: current_user.most_used_project_id)
-    @tracking.billable = true
-
+    @show_actions = true
     @trackings = current_user.trackings.where("start_time >= ? OR updated_at >= ?", Date.today, Date.today-1.day)
                              .group_by(&:group_by_criteria).map { |d,t| [d, t] }
-    @projects = Project.all
+  end
+
+  private
+
+  def set_data_for_new_tracking
+    @tracking = Tracking.new(project_id: current_user.most_used_project_id)
+    @tracking.billable = true
+    @projects = Project.all 
+  end
+
+  def greeting
+    ["Hey there","Bon dia","Aloha","Hallo","Hi","Ola","Hola","Namaste", "Moin Moin"].sample
   end
 
 end
