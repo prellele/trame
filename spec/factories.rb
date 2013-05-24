d970b3ddd1c16ce1b97bdfbe443afa3e0ce26074
Lennart Prelle <me@prellele.de>
2013-05-24 15:19:47 +0200

    add some tests WIP

--- spec/factories.rb
+++ spec/factories.rb
@@ -14,4 +14,30 @@ FactoryGirl.define do
     password_confirmation "00test00"
     admin                 true
   end
+
+  factory :awesomeproject, class: Project do
+    name                  "Awesomeproject"
+  end
+
+  factory :niceproject, class: Project do
+    name                 "Niceproject"
+  end
+
+  factory :tracking_awesomeproject, class: Tracking do
+    description           "Build an awesome webproject"
+    start_time            "2013-04-08 13:00:00"
+    minutes               120
+    billable              true
+    user_id               1
+    project_id            1
+  end
+
+  factory :tracking_niceproject, class: Tracking do
+    description           "Build a nice webproject"
+    start_time            "2013-04-08 13:00:00"
+    minutes               120
+    billable              true
+    user_id               1
+    project_id            2
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,12 @@ require 'spec_helper'
 describe User do
   user = FactoryGirl.build(:user)
   admin = FactoryGirl.build(:admin)
+  awesomeproject = FactoryGirl.build(:awesomeproject)
+  tracking = FactoryGirl.build(:tracking_awesomeproject)
 
   describe "validation" do
     describe "of email" do
+
       it "requires presence" do
         user.email = nil
         user.should_not be_valid
@@ -20,7 +23,18 @@ describe User do
         user.email = "somebody@anywhere"
         user.should_not be_valid
       end
+
     end
   end
-  
+
+  describe "return value" do
+    describe "of workinghours" do
+
+      it "is right" do
+        expect(user.workinghours()).to eq(120)
+      end
+
+    end
+  end
+   
 end
