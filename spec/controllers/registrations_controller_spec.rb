d042b371d61310b7aab91e2d85d3326ca3adec1c
Lennart Prelle <me@prellele.de>
2013-06-07 16:26:48 +0200

    add tests WIP

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -6,23 +6,7 @@ describe RegistrationsController do
 
   describe "#update" do
 
-    describe "email" do
-      before do
-        @paramsUser = { :id => user.id, :email => "testchange@prellele.de" }
-        @params = { :utf8 => "✓", :method=> "put",
-                  :user => { :id => user.id, :email => "testchange@prellele.de" }  }
-
-      end
-      it "does overwrite email without current password" do
-        user.update_without_password(@paramsUser).should change(user, :email)
-      end
-
-      it "does overwrite email without current password" do
-        lambda {
-          put :update, @params
-        }.should change(user, :email)
-      end
-    end
+    
   end
   
 end
